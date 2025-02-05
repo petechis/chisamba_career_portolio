83a5c0ee526451cc498e8f8c4f1590a882c856f5
Pete Chisamba <chisamba2010@yahoo.co.uk>
2025-02-05 12:28:49 +0100

    Added animated scrolling to navigation and scrolling between sections ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,15 +34,16 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
+sr.reveal('.home__title',{});  
+sr.reveal('.home-text-column',{delay:200});
 sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home-image-column',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
+sr.reveal('.about-image-column',{delay:400}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about-text-column',{delay: 400}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
